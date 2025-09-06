3cdb4f712e9deb5c362426319ca37a4123611bf9
Alexandre Sousa Silva <pseudoalexandre@gmail.com>
2025-09-06 00:17:12 -0300

    chore: add lint-staged, commitlint and configure ESLint to code formatting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,12 @@
 import nx from '@nx/eslint-plugin';
+import prettierPlugin from 'eslint-plugin-prettier';
 
 export default [
   ...nx.configs['flat/base'],
   ...nx.configs['flat/typescript'],
   ...nx.configs['flat/javascript'],
   {
-    ignores: ['**/dist'],
+    ignores: ['**/dist', 'eslint.config.mjs'],
   },
   {
     files: [
@@ -18,8 +19,45 @@ export default [
       '**/*.cjs',
       '**/*.mjs',
     ],
-    // Override or add rules here
-    rules: {},
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    languageOptions: {
+      parserOptions: {
+        project: ['tsconfig.json', 'tsconfig.app.json', 'tsconfig.spec.json'],
+        createDefaultProgram: true,
+      },
+    },
+    rules: {
+      'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+      'no-console': 'error',
+      '@typescript-eslint/explicit-function-return-type': 'error',
+      '@typescript-eslint/prefer-readonly': 'error',
+      '@typescript-eslint/no-inferrable-types': 'error',
+      '@typescript-eslint/naming-convention': [
+        'warn',
+        {
+          selector: 'interface',
+          format: ['PascalCase'],
+          custom: {
+            regex: '^[iI][A-Z]',
+            match: false,
+          },
+        },
+        {
+          selector: 'typeAlias',
+          format: ['PascalCase'],
+        },
+        {
+          selector: 'enum',
+          format: ['PascalCase'],
+        },
+        {
+          selector: 'enumMember',
+          format: ['UPPER_CASE'],
+        },
+      ],
+    },
   },
   ...nx.configs['flat/angular'],
   ...nx.configs['flat/angular-template'],
@@ -46,7 +84,9 @@ export default [
   },
   {
     files: ['**/*.html'],
-    // Override or add rules here
-    rules: {},
+    rules: {
+      '@angular-eslint/template/button-has-type': 'error',
+      '@angular-eslint/template/no-positive-tabindex': 'error',
+    },
   },
 ];
