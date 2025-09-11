a49cab26d8ea53d8e173e7bb23617e8202ddd3cd
Alexandre Sousa Silva <pseudoalexandre@gmail.com>
2025-09-10 23:32:04 -0300

    test: add basic test for app component

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,19 +1,23 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { App } from './app';
 
 describe('App', () => {
+  let component: App;
+  let fixture: ComponentFixture<App>;
+
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
+    TestBed.configureTestingModule({
       imports: [App],
-    }).compileComponents();
+    });
+
+    await TestBed.compileComponents();
+
+    fixture = TestBed.createComponent(App);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(App);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome task-manager',
-    );
+  it('should create the component', () => {
+    expect(component).toBeTruthy();
   });
 });
