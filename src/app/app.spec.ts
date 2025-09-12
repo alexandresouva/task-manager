37f5b673848a565fcb8fdbfc7d0097d189f9216a
Alexandre Sousa Silva <pseudoalexandre@gmail.com>
2025-09-11 23:32:27 -0300

    test(app): add tests for app-header and router-outlet in the template

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { App } from './app';
+import { By } from '@angular/platform-browser';
 
 describe('App', () => {
   let component: App;
@@ -19,5 +20,15 @@ describe('App', () => {
 
   it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have app-header in the template', () => {
+    const appHeaderEl = fixture.debugElement.query(By.css('app-header'));
+    expect(appHeaderEl).toBeTruthy();
+  });
+
+  it('should have router-outlet in the template', () => {
+    const routerOutletEl = fixture.debugElement.query(By.css('router-outlet'));
+    expect(routerOutletEl).toBeTruthy();
   });
 });
