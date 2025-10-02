871af2f4da45cefdeec0877bd5ed32d57d160f8a
Alexandre Sousa Silva <pseudoalexandre@gmail.com>
2025-10-02 00:18:30 -0300

    chore: remove i18n implementation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,7 +6,6 @@ import {
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
-import { provideI18n } from './core/i18n/providers/i18n.providers';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,6 +13,5 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
     provideHttpClient(),
-    provideI18n(),
   ],
 };
--- src/app/core/i18n/constants/languages.ts
+++
@@ -1,4 +0,0 @@
-import { LanguageCode } from '@core/i18n/models/language.model';
-
-export const AVAILABLE_LANGUAGES: LanguageCode[] = ['en-US', 'pt-BR', 'es-ES'];
-export const FALLBACK_LANGUAGE: LanguageCode = 'en-US';
