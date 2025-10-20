8d1e90f4753002fccf7cce477ef12de8076ab4ea
Alexandre Sousa Silva <pseudoalexandre@gmail.com>
2025-10-19 21:22:11 -0300

    refactor: group HTTP request expectations into a single object for clarity and consistency

--- src/app/shared/services/task-service.spec.ts
+++ src/app/shared/services/task-service.spec.ts
@@ -30,41 +30,46 @@ describe('TaskService', () => {
 
   // Method 1: fakeAsync
   it('should return tasks - using fakeAsync', fakeAsync(() => {
-    let result: Task[] | undefined;
+    let result: Task[] | null = null;
 
     service.getAll().subscribe((tasks) => {
       result = tasks;
     });
 
-    const req = httpTestingController.expectOne(environment.endpoints.tasks);
+    const req = httpTestingController.expectOne({
+      url: environment.endpoints.tasks,
+      method: 'GET',
+    });
     req.flush(tasksMock);
+    tick();
 
-    tick(2000);
-
-    expect(req.request.method).toBe('GET');
     expect(result).toStrictEqual(tasksMock);
   }));
 
   // Method 2: done
   it('should return tasks - using done', (done) => {
     service.getAll().subscribe((tasks) => {
-      expect(req.request.method).toBe('GET');
       expect(tasks).toStrictEqual(tasksMock);
       done();
     });
 
-    const req = httpTestingController.expectOne(environment.endpoints.tasks);
+    const req = httpTestingController.expectOne({
+      url: environment.endpoints.tasks,
+      method: 'GET',
+    });
     req.flush(tasksMock);
   });
 
   // Method 3: waitForAsync
   it('should return tasks - using waitForAsync', waitForAsync(() => {
     service.getAll().subscribe((tasks) => {
-      expect(req.request.method).toBe('GET');
       expect(tasks).toStrictEqual(tasksMock);
     });
 
-    const req = httpTestingController.expectOne(environment.endpoints.tasks);
+    const req = httpTestingController.expectOne({
+      url: environment.endpoints.tasks,
+      method: 'GET',
+    });
     req.flush(tasksMock);
   }));
 
@@ -72,26 +77,25 @@ describe('TaskService', () => {
     const updatedTask: Task = { id: 2, description: 'Task 2', completed: true };
 
     service.update(updatedTask.id, updatedTask).subscribe((task) => {
-      expect(req.request.method).toBe('PUT');
       expect(task).toStrictEqual(updatedTask);
     });
 
-    const req = httpTestingController.expectOne(
-      `${environment.endpoints.tasks}/${updatedTask.id}`,
-    );
+    const req = httpTestingController.expectOne({
+      url: `${environment.endpoints.tasks}/${updatedTask.id}`,
+      method: 'PUT',
+    });
     req.flush(updatedTask);
   });
 
   it('should delete a task', fakeAsync(() => {
     const taskIdToDelete = 3;
 
-    service.delete(taskIdToDelete).subscribe(() => {
-      expect(req.request.method).toBe('DELETE');
-    });
+    service.delete(taskIdToDelete).subscribe();
 
-    const req = httpTestingController.expectOne(
-      `${environment.endpoints.tasks}/${taskIdToDelete}`,
-    );
+    const req = httpTestingController.expectOne({
+      url: `${environment.endpoints.tasks}/${taskIdToDelete}`,
+      method: 'DELETE',
+    });
     req.flush(null);
   }));
 });
