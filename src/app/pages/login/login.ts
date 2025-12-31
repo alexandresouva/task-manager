6bfbd9fc1c5f9cec78fd7f64dd7d66adc73e9607
Alexandre Sousa Silva <pseudoalexandre@gmail.com>
2025-12-30 23:05:05 -0300

    feat: display error toast when login fails

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,9 +1,10 @@
+import { HttpErrorResponse, HttpStatusCode } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { AuthService } from '@shared/services/auth-service';
-import { tap } from 'rxjs';
+import { ToastService } from '@shared/services/toast-service';
 
 @Component({
   selector: 'app-login',
@@ -14,7 +15,9 @@ export class Login {
   private readonly authService = inject(AuthService);
   private readonly fb = inject(FormBuilder);
   private readonly router = inject(Router);
+  private readonly toastService = inject(ToastService);
 
+  /* Initialize with correct credentials to easily test successful login */
   readonly loginForm = this.fb.nonNullable.group({
     email: ['fake@email.com', [Validators.required, Validators.email]],
     password: ['fakePassword', Validators.required],
@@ -23,14 +26,31 @@ export class Login {
   protected login(): void {
     if (this.loginForm.invalid) {
       this.loginForm.markAllAsTouched();
+      this.toastService.show({
+        type: 'error',
+        title: 'Error',
+        message: 'Please fill in all required fields correctly.',
+      });
       return;
     }
 
     const { email, password } = this.loginForm.getRawValue();
 
-    this.authService
-      .login(email, password)
-      .pipe(tap(() => this.router.navigateByUrl('tasks')))
-      .subscribe();
+    this.authService.login(email, password).subscribe({
+      next: () => void this.router.navigateByUrl('tasks'),
+      error: (e) => this.onLoginError(e),
+    });
+  }
+
+  private onLoginError(error: HttpErrorResponse): void {
+    const isUnauthorized = error.status === HttpStatusCode.Unauthorized;
+
+    this.toastService.show({
+      type: 'error',
+      title: 'Error',
+      message: isUnauthorized
+        ? 'Invalid email or password.'
+        : 'An error occurred during login.',
+    });
   }
 }
