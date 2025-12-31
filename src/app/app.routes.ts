53c9675fc4c67ced9e2b31e081ff4988309db625
Alexandre Sousa Silva <pseudoalexandre@gmail.com>
2025-12-31 14:37:08 -0300

    feat: include authentication guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,29 @@
 import { Route } from '@angular/router';
 
 import { getTaskByIdResolver } from '@pages/edit/resolvers/get-task-by-id-resolver';
+import { isLoggedInGuard } from '@shared/guards/is-logged-in-guard';
 
 export const appRoutes: Route[] = [
-  { path: '', redirectTo: 'tasks', pathMatch: 'full' },
   {
     path: 'login',
     loadComponent: () => import('./pages/login/login').then((m) => m.Login),
   },
   {
-    path: 'tasks',
-    loadComponent: () => import('./pages/list/list').then((m) => m.List),
-  },
-  {
-    path: 'tasks/:id/edit',
-    loadComponent: () => import('./pages/edit/edit').then((m) => m.Edit),
-    resolve: {
-      task: getTaskByIdResolver,
-    },
+    path: '',
+    canActivateChild: [isLoggedInGuard],
+    children: [
+      { path: '', redirectTo: 'tasks', pathMatch: 'full' },
+      {
+        path: 'tasks',
+        loadComponent: () => import('./pages/list/list').then((m) => m.List),
+      },
+      {
+        path: 'tasks/:id/edit',
+        loadComponent: () => import('./pages/edit/edit').then((m) => m.Edit),
+        resolve: {
+          task: getTaskByIdResolver,
+        },
+      },
+    ],
   },
 ];
