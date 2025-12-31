7d6f0447f48ceda9058a0cfa9ca20969a5fa0572
Alexandre Sousa Silva <pseudoalexandre@gmail.com>
2025-12-31 12:43:43 -0300

    test: integrate auth store in login component to handle authentication state

--- src/app/pages/login/login.spec.ts
+++ src/app/pages/login/login.spec.ts
@@ -6,6 +6,7 @@ import { provideRouter } from '@angular/router';
 import { List } from '@pages/list/list';
 import { AuthService } from '@shared/services/auth-service';
 import { ToastService } from '@shared/services/toast-service';
+import { AuthStore } from '@shared/stores/auth-store';
 import { TestHelper } from '@testing/helpers/test-helper';
 import { MockComponent, MockService } from 'ng-mocks';
 import { of, throwError } from 'rxjs';
@@ -14,6 +15,7 @@ import { Login } from './login';
 
 function setup() {
   const authServiceMock = MockService(AuthService) as jest.Mocked<AuthService>;
+  const authStoreMock = MockService(AuthStore) as jest.Mocked<AuthStore>;
   const toastServiceMock = MockService(
     ToastService,
   ) as jest.Mocked<ToastService>;
@@ -23,6 +25,7 @@ function setup() {
     providers: [
       provideRouter([{ path: 'tasks', component: MockComponent(List) }]),
       { provide: AuthService, useValue: authServiceMock },
+      { provide: AuthStore, useValue: authStoreMock },
       { provide: ToastService, useValue: toastServiceMock },
     ],
   });
@@ -33,7 +36,14 @@ function setup() {
 
   fixture.detectChanges();
 
-  return { fixture, component, testHelper, authServiceMock, toastServiceMock };
+  return {
+    fixture,
+    component,
+    testHelper,
+    authServiceMock,
+    authStoreMock,
+    toastServiceMock,
+  };
 }
 
 function fillAndSubmitForm(
@@ -81,7 +91,7 @@ describe('Login', () => {
     });
 
     it('should login and navigate to tasks page if success', fakeAsync(() => {
-      const { testHelper, authServiceMock } = setup();
+      const { testHelper, authServiceMock, authStoreMock } = setup();
       const location = TestBed.inject(Location);
 
       authServiceMock.login.mockReturnValue(of({ token: 'fake_jwt_token' }));
@@ -94,11 +104,13 @@ describe('Login', () => {
 
       tick();
 
+      expect(authStoreMock.isAuthenticated).toBe(true);
       expect(location.path()).toBe('/tasks');
     }));
 
     it('should display toast error and keep on login page if login fails', fakeAsync(() => {
-      const { testHelper, authServiceMock, toastServiceMock } = setup();
+      const { testHelper, authServiceMock, authStoreMock, toastServiceMock } =
+        setup();
       const location = TestBed.inject(Location);
       const unauthorizedFakeError = new HttpErrorResponse({
         status: 401,
@@ -118,11 +130,40 @@ describe('Login', () => {
 
       tick();
 
+      expect(authStoreMock.isAuthenticated).toBe(false);
       expect(location.path()).toBe('');
       expect(toastServiceMock.show).toHaveBeenCalledWith({
         type: 'error',
         title: 'Error',
         message: 'Invalid email or password.',
+      });
+    }));
+
+    it('should display generic toast error if a different error occurs', fakeAsync(() => {
+      const { testHelper, authServiceMock, authStoreMock, toastServiceMock } =
+        setup();
+      const location = TestBed.inject(Location);
+      const serverFakeError = new HttpErrorResponse({
+        status: 500,
+        statusText: 'Server Error',
+      });
+
+      authServiceMock.login.mockReturnValue(throwError(() => serverFakeError));
+      fillAndSubmitForm(testHelper, validEmail, validPassword);
+
+      expect(authServiceMock.login).toHaveBeenCalledWith(
+        validEmail,
+        validPassword,
+      );
+
+      tick();
+
+      expect(authStoreMock.isAuthenticated).toBe(false);
+      expect(location.path()).toBe('');
+      expect(toastServiceMock.show).toHaveBeenCalledWith({
+        type: 'error',
+        title: 'Error',
+        message: 'An error occurred during login. Please try again later.',
       });
     }));
   });
