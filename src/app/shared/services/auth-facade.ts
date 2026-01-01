6e5461c5467dfc9bf259be0338f1dfcaeac640f0
Alexandre Sousa Silva <pseudoalexandre@gmail.com>
2026-01-01 12:38:49 -0300

    feat: handle auth token in localStorage

--- src/app/shared/services/auth-facade.ts
+++ src/app/shared/services/auth-facade.ts
@@ -4,6 +4,7 @@ import { AuthStore } from '@shared/stores/auth-store';
 import { catchError, map, Observable, tap, throwError } from 'rxjs';
 
 import { AuthService } from './auth-service';
+import { AuthStorageService } from './auth-storage-service';
 
 @Injectable({
   providedIn: 'root',
@@ -11,13 +12,16 @@ import { AuthService } from './auth-service';
 export class AuthFacade {
   private readonly authService = inject(AuthService);
   private readonly authStore = inject(AuthStore);
+  private readonly authStorage = inject(AuthStorageService);
 
   login(email: string, password: string): Observable<void> {
     return this.authService.login(email, password).pipe(
       tap(() => (this.authStore.isAuthenticated = true)),
+      tap(({ token }) => this.authStorage.setToken(token)),
       map(() => void 0),
       catchError((error) => {
         this.authStore.isAuthenticated = false;
+        this.authStorage.clearToken();
         return throwError(() => error);
       }),
     );
---
+++ src/app/shared/services/auth-storage-service.ts
@@ -0,0 +1,24 @@
+import { inject, Injectable } from '@angular/core';
+
+import { LOCAL_STORAGE } from '@shared/tokens/local-storage.token';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthStorageService {
+  private readonly localStorage = inject(LOCAL_STORAGE);
+
+  readonly #tokenKey = 'auth_token';
+
+  setToken(token: string): void {
+    this.localStorage.setItem(this.#tokenKey, token);
+  }
+
+  getToken(): string | null {
+    return this.localStorage.getItem(this.#tokenKey);
+  }
+
+  clearToken(): void {
+    this.localStorage.removeItem(this.#tokenKey);
+  }
+}
