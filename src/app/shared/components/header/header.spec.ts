2d954efcae11817b85d5909514f8be8aebe9d20f
Alexandre Sousa Silva <pseudoalexandre@gmail.com>
2026-01-04 00:46:49 -0300

    test: implement profile component and integrate with logout functionality

--- src/app/shared/components/header/header.spec.ts
+++ src/app/shared/components/header/header.spec.ts
@@ -1,12 +1,26 @@
+import { signal } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 
+import { AuthStore } from '@shared/stores/auth-store';
+import { MockService } from 'ng-mocks';
+
 import { Header } from './header';
 
-async function setup() {
+type SetupParams = {
+  isAuthenticated: boolean;
+};
+
+async function setup({ isAuthenticated = false }: Partial<SetupParams> = {}) {
+  const authStoreMock = MockService(AuthStore) as jest.Mocked<AuthStore>;
+  Object.defineProperty(authStoreMock, 'isAuthenticated', {
+    value: signal(isAuthenticated),
+  });
+
   // Configure the testing module
   TestBed.configureTestingModule({
     imports: [Header],
+    providers: [{ provide: AuthStore, useValue: authStoreMock }],
   });
   // Compile the components (external templates and styles)
   await TestBed.compileComponents();
@@ -15,7 +29,7 @@ async function setup() {
   const component = fixture.componentInstance;
   fixture.detectChanges();
 
-  return { component, fixture };
+  return { component, fixture, authStoreMock };
 }
 
 describe('Header', () => {
@@ -30,5 +44,23 @@ describe('Header', () => {
     const h1Content = h1DebugEl.nativeElement.textContent;
 
     expect(h1Content).toBe('Task Manager');
+  });
+
+  describe('when user is authenticated', () => {
+    it('should render profile component', async () => {
+      const { fixture } = await setup({ isAuthenticated: true });
+
+      const profileDebugEl = fixture.debugElement.query(By.css('app-profile'));
+      expect(profileDebugEl).toBeTruthy();
+    });
+  });
+
+  describe('when user is not authenticated', () => {
+    it('should not render profile component', async () => {
+      const { fixture } = await setup({ isAuthenticated: false });
+
+      const profileDebugEl = fixture.debugElement.query(By.css('app-profile'));
+      expect(profileDebugEl).toBeFalsy();
+    });
   });
 });
---
+++ src/app/shared/components/profile/profile.spec.ts
@@ -0,0 +1,109 @@
+import { Location } from '@angular/common';
+import { Component } from '@angular/core';
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
+
+import { AuthFacade } from '@core/auth/services/auth-facade';
+import { TestHelper } from '@testing/helpers/test-helper';
+import { MockComponent, MockService } from 'ng-mocks';
+import { of } from 'rxjs';
+
+import { Profile } from './profile';
+
+@Component({
+  selector: 'app-fake-login',
+  template: '<div>Mock Login Component</div>',
+})
+class FakeLoginComponent {}
+
+function setup() {
+  const authFacadeMock = MockService(AuthFacade) as jest.Mocked<AuthFacade>;
+
+  TestBed.configureTestingModule({
+    imports: [Profile],
+    providers: [
+      { provide: AuthFacade, useValue: authFacadeMock },
+      provideRouter([
+        { path: 'login', component: MockComponent(FakeLoginComponent) },
+      ]),
+    ],
+  }).compileComponents();
+
+  const fixture = TestBed.createComponent(Profile);
+  const component = fixture.componentInstance;
+  fixture.detectChanges();
+  const testHelper = new TestHelper(fixture);
+  const location = TestBed.inject(Location);
+
+  return { component, fixture, testHelper, authFacadeMock, location };
+}
+
+describe('Profile', () => {
+  it('should create', () => {
+    const { component } = setup();
+    expect(component).toBeTruthy();
+  });
+
+  describe('when menu button is clicked', () => {
+    it('should toggle menu visibility', () => {
+      const { fixture, testHelper } = setup();
+
+      let menu = testHelper.queryByTestId('profile-menu');
+      expect(menu).toBeNull();
+
+      testHelper.triggerClickByTestId('profile-menu-button');
+      fixture.detectChanges();
+
+      menu = testHelper.queryByTestId('profile-menu');
+      expect(menu).toBeTruthy();
+    });
+  });
+
+  describe('when clicking outside the component', () => {
+    it('should close the menu', () => {
+      const { fixture, testHelper } = setup();
+
+      testHelper.triggerClickByTestId('profile-menu-button');
+      fixture.detectChanges();
+
+      let menu = testHelper.queryByTestId('profile-menu');
+      expect(menu).toBeTruthy();
+
+      const event = new MouseEvent('pointerdown', {
+        bubbles: true,
+        cancelable: true,
+        composed: true,
+        clientX: 0,
+        clientY: 0,
+      });
+      document.dispatchEvent(event);
+      fixture.detectChanges();
+
+      menu = testHelper.queryByTestId('profile-menu');
+      expect(menu).toBeNull();
+    });
+  });
+
+  describe('when select the logout option', () => {
+    it('should close the menu, start the logout process and redirect to the login page', fakeAsync(() => {
+      const { fixture, testHelper, authFacadeMock, location } = setup();
+      authFacadeMock.logout.mockReturnValue(of(void 0));
+
+      testHelper.triggerClickByTestId('profile-menu-button');
+      fixture.detectChanges();
+
+      let menu = testHelper.queryByTestId('profile-menu');
+      expect(menu).toBeTruthy();
+
+      testHelper.triggerClickByTestId('logout-option');
+      fixture.detectChanges();
+      tick();
+
+      menu = testHelper.queryByTestId('profile-menu');
+
+      expect(menu).toBeNull();
+      expect(authFacadeMock.logout).toHaveBeenCalled();
+      expect(location.path()).toBe('/login');
+    }));
+  });
+});
