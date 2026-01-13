c7b09f7407bc2ab2f00bb6d301a602f0bb91d3f6
Alexandre Sousa Silva <pseudoalexandre@gmail.com>
2026-01-12 23:39:46 -0300

    refactor: use credentials fixture to organize login mock data

--- e2e/src/e2e/login.cy.ts
+++ e2e/src/e2e/login.cy.ts
@@ -7,9 +7,11 @@ describe('auth flow', () => {
     it('should redirect to login', () => {
       cy.location('pathname').should('eq', '/login');
 
-      cy.get('[data-testid="login-email"]').clear().type('fake@email.com');
-      cy.get('[data-testid="login-password"]').clear().type('fakePassword');
-      cy.get('[data-testid="login-button"]').click();
+      cy.fixture('credentials').then(({ email, password }) => {
+        cy.get('[data-testid="login-email"]').clear().type(email);
+        cy.get('[data-testid="login-password"]').clear().type(password);
+        cy.get('[data-testid="login-button"]').click();
+      });
 
       cy.location('pathname').should('eq', '/tasks');
     });
