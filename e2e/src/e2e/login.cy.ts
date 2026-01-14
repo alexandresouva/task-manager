f4b4b4f64775d088bf9b11c999c393ef8627f108
Alexandre Sousa Silva <pseudoalexandre@gmail.com>
2026-01-13 21:38:58 -0300

    refactor: standardize element selection using getByTestId

--- e2e/src/e2e/login.cy.ts
+++ e2e/src/e2e/login.cy.ts
@@ -8,9 +8,9 @@ describe('auth flow', () => {
       cy.location('pathname').should('eq', '/login');
 
       cy.fixture('credentials').then(({ email, password }) => {
-        cy.get('[data-testid="login-email"]').clear().type(email);
-        cy.get('[data-testid="login-password"]').clear().type(password);
-        cy.get('[data-testid="login-button"]').click();
+        cy.getByTestId('login-email').clear().type(email);
+        cy.getByTestId('login-password').clear().type(password);
+        cy.getByTestId('login-button').click();
       });
 
       cy.location('pathname').should('eq', '/tasks');
